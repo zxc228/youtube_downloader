ac8a9168ba7a5aa6a65a207351876e762b31d3b4
Ilya <ilya423@yandex.ru>
2025-02-23 20:53:17 +0100

    new v

--- downloader.py
+++ downloader.py
@@ -1,10 +1,22 @@
 import yt_dlp
+import os
 
-def download_video_yt_dlp(url: str, output_path: str = "downloads"):
+def download_video_yt_dlp(url: str, output_folder: str = "downloads"):
+    # Создаем папку, если её нет
+    os.makedirs(output_folder, exist_ok=True)
+    
     ydl_opts = {
-        "outtmpl": f"{output_path}/%(title)s.%(ext)s",
-        "format": "bestvideo+bestaudio/best"
+        "outtmpl": os.path.join(output_folder, "%(title)s.%(ext)s"),  # Имя файла по названию видео
+        "format": "bestvideo[height<=720][ext=mp4][vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4]",
+        "merge_output_format": "mp4",
+        "noplaylist": True,
+        "geo_bypass": True,
+        "quiet": False,
+        "postprocessors": [{
+            "key": "FFmpegMetadata"
+        }]
     }
+    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
